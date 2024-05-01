276de8d7d9303187326ae7560b1a738599f02d67
cricky04 <shyun00124@naver.com>
2024-05-01 13:19:08 +0900

    feat: BA & ER graph generator

--- engine/data/dataGenerator..py
+++ engine/data/dataGenerator..py
@@ -1,6 +1,34 @@
+'''
+for artificial data(theorical research)
+'''
+
 import torch
-from torch_geometric.datasets.graph_generator import BAGraph
+from torch_geometric.datasets.graph_generator import BAGraph, ERGraph
 
 # Barabasi-Albert graph generator
-def BAGenerator(node, edge):
-    pass
+def BAGenerator(num_nodes, num_edges):
+    '''
+    input
+        num_nodes: number of nodes
+        num_edges: number of edges
+    output
+        data: torch_geometry.Data
+    '''
+    # TODO: add node features
+    baGraph = BAGraph(num_nodes, num_edges)
+    
+    return baGraph()
+
+def ERGenerator(num_nodes, prob_edges):
+    '''
+    input
+        num_nodes: number of nodes
+        prob_edges: probability of edges
+    output
+        data: torch_geometry.Data
+    '''
+    # TODO: add node features
+    erGraph = ERGraph(num_nodes, prob_edges)
+    
+    return erGraph() 
+    
